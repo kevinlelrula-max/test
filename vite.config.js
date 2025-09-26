3006693febb43e67aeaf746edda8d2dac08ef8fc
unknown <kevinlelrula@gmail.com>
2025-09-25 22:45:59 -0500

    Forzar actualización de index.html y supportedFrameworks.html

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
+  base: './',
+})
